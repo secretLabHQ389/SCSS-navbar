8d98eba91860288c9e65e69c9384312987f252fc
secretLabHQ389 <tech_solutions289@protonmail.com>
2022-11-24 19:40:26 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.scss";
+import Navbar from "./components/navbar/Navbar"
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
