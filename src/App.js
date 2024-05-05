ea9ed6763519da7e5a0275f0b8c263945c507eb3
George Payne IV <85563814+secretLabHQ389@users.noreply.github.com>
2024-05-05 20:47:03 +1000

    feat(module build): navigation, SCSS updates, module hover and features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import { useState } from "react";
-import "./App.scss";
-import Navbar from "./components/navbar/Navbar"
+import React, { useState, Suspense, lazy } from 'react';
+import './App.scss'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import CssExs from './components/items/CssExs'
+import Greeting from './components/items/Greeting'
+import Loader from './components/items/Loader'
 
 function App() {
+  const [showGreeting, setShowGreeting ] = useState(false)
+
+  const showAlertGreetingHandler = () => {
+    setShowGreeting(showGreeting => !showGreeting)
+  }
+
   return (
-    <Navbar />
-  );
+    <>
+      <Navbar />
+      <button type="submit" onClick={() => showAlertGreetingHandler()}>Greetings Visitor</button>
+      {showGreeting && (
+        <Suspense fallback={<Loader />}>
+          <Greeting />
+        </Suspense>
+      )}
+      <CssExs />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -8,16 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// import React from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App";
-
-// const container = document.getElementById("root");
-// const root = createRoot(container);
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
